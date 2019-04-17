f6ff33c68908bc5ac2305472f6c612f0c9698184
sarahchou <chou.s@husky.neu.edu>
2019-04-17 14:34:38 -0400

    added in winner checking

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -21,6 +21,10 @@ function Square(props) {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
+        //return early by ignoring click if someone won the game or a square is already filled
+        if (calculateWinner(squares) || squares[i]) {
+          return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
           squares: squares,
@@ -38,7 +42,14 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      }
+      else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
@@ -86,4 +97,22 @@ function Square(props) {
     document.getElementById('root')
   );
   
-  
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i ++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
