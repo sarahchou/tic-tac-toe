8f708863dfb696512a9ee3ccb145f9428d914029
sarahchou <chou.s@husky.neu.edu>
2019-04-17 14:44:10 -0400

    updating Game's render to have history and status

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -78,13 +78,28 @@ function Square(props) {
       };
     }
     render() {
+      const history = this.state.history;
+      const current = history[history.length - 1];
+      const winner = calculateWinner(current.squares);
+
+      let status;
+      if (winner) {
+        states = 'Winner: ' + winner;
+      }
+      else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+
       return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board 
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
+            <div>{status}</div>
             <ol>{/* TODO */}</ol>
           </div>
         </div>
